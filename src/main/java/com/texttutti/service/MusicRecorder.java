96785ace8183a49a8f9a07a44111dbb01e9818c6
Phillip-Taylor <phillip.taylor@autotrader.co.uk>
2018-10-27 22:50:32 +0100

    Change test file in case I'm going mad

--- src/main/java/com/texttutti/service/MusicRecorder.java
+++ src/main/java/com/texttutti/service/MusicRecorder.java
@@ -18,18 +18,18 @@ public class MusicRecorder {
         final Score score = new Score("Test score");
         final Part part = new Part();
         final Phrase phrase = new Phrase("Honky Tonk", 0.0, 3);
-        phrase.add(new Note(62, JMC.CROTCHET));
         phrase.add(new Note(64, JMC.CROTCHET));
         phrase.add(new Note(66, JMC.CROTCHET));
-        phrase.add(new Note(67, JMC.CROTCHET));
+        phrase.add(new Note(68, JMC.CROTCHET));
         phrase.add(new Note(69, JMC.CROTCHET));
         phrase.add(new Note(71, JMC.CROTCHET));
         phrase.add(new Note(73, JMC.CROTCHET));
         phrase.add(new Note(74, JMC.CROTCHET));
+        phrase.add(new Note(76, JMC.CROTCHET));
         part.add(phrase);
         score.add(part);
         try {
-            final String pathname = new File(".").getCanonicalPath() + "//scores//testScore.mid";
+            final String pathname = new File(".").getCanonicalPath() + "//scores//testScore2.mid";
             File file = new File(pathname);
             file.getParentFile().mkdirs();
             file.createNewFile();
