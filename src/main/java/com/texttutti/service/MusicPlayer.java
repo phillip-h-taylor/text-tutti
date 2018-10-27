a17a95b46d7ee21786261a1c9364d08c34e0907f
Phillip-Taylor <phillip.taylor@autotrader.co.uk>
2018-10-27 18:55:23 +0100

    Reinstated playing of music, hopefully it works better this time!

---
+++ src/main/java/com/texttutti/service/MusicPlayer.java
@@ -0,0 +1,20 @@
+package com.texttutti.service;
+
+import org.jfugue.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MusicPlayer {
+
+    private final Player player;
+
+    @Autowired
+    public MusicPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void playSomething() {
+        player.play("C D E F G A B");
+    }
+}
