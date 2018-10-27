07c97f17dea3f90a81b9e9ac225aa80207137144
Phillip-Taylor <phillip.taylor@autotrader.co.uk>
2018-10-27 20:06:56 +0100

    Record music and save to MIDI file

--- src/main/java/com/texttutti/service/MessageService.java
+++ src/main/java/com/texttutti/service/MessageService.java
@@ -6,14 +6,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
     private final MessageSender messageSender;
+    private final MusicRecorder musicRecorder;
 
     @Autowired
-    public MessageService(MessageSender messageSender) {
+    public MessageService(MessageSender messageSender, MusicRecorder musicRecorder) {
         this.messageSender = messageSender;
+        this.musicRecorder = musicRecorder;
     }
 
     public void processMessage(String id, Long from, String content) {
+        musicRecorder.recordSomething();
         messageSender.sendMessage(from, String.format("Message received successfully. Original content: %s", content));
     }
 }
---
+++ src/main/java/com/texttutti/service/MusicRecorder.java
@@ -0,0 +1,34 @@
+package com.texttutti.service;
+
+import jm.JMC;
+import jm.music.data.Note;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+import jm.util.Write;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+@Component
+public class MusicRecorder {
+
+    public void recordSomething() {
+        final Score score = new Score("Test score");
+        final Part part = new Part();
+        final Phrase phrase = new Phrase("Honky Tonk", 0.0, 3);
+        phrase.add(new Note(62, JMC.CROTCHET));
+        phrase.add(new Note(64, JMC.CROTCHET));
+        phrase.add(new Note(66, JMC.CROTCHET));
+        phrase.add(new Note(67, JMC.CROTCHET));
+        phrase.add(new Note(69, JMC.CROTCHET));
+        phrase.add(new Note(71, JMC.CROTCHET));
+        phrase.add(new Note(73, JMC.CROTCHET));
+        phrase.add(new Note(74, JMC.CROTCHET));
+        part.add(phrase);
+        score.add(part);
+        Write.midi(score, "/testScore.mid");
+    }
+
+}
