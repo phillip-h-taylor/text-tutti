26618131459ba44bf20c99c973e884c5d554e3bd
Phillip-Taylor <phillip.taylor@autotrader.co.uk>
2018-10-27 14:30:40 +0100

    Initial commit

---
+++ src/main/java/com/texttutti/texttutti/TexttuttiApplication.java
@@ -0,0 +1,12 @@
+package com.texttutti.texttutti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexttuttiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexttuttiApplication.class, args);
+	}
+}
