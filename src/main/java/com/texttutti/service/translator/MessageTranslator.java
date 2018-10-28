864de8d284524013dff2f6255e551c47a4e59565
Phillip-Taylor <phillip.taylor@autotrader.co.uk>
2018-10-28 03:02:46 +0000

    Added functionality for multiple parts.

--- src/main/java/com/texttutti/service/translator/MessageTranslator.java
+++ src/main/java/com/texttutti/service/translator/MessageTranslator.java
@@ -4,13 +4,11 @@ import com.texttutti.service.model.TranslationResponse;
 import jm.music.data.Part;
 import jm.music.data.Phrase;
 import jm.music.data.Score;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 @Component
 public class MessageTranslator {
@@ -23,37 +21,43 @@ public class MessageTranslator {
     }
 
     public TranslationResponse translateMessage(String content) throws TranslationException {
-        String fileName = "score.mid";
-        final List<String> components = Arrays.asList(content.split(","));
         final Score score = new Score();
-        final Part part = new Part();
-        final Phrase phrase = new Phrase();
-        try {
+        String fileName = "score.mid";
+        final List<String> parts = Arrays.asList(content.split("&"));
+        int channel = 0;
+        for (String partString : parts) {
+            final List<String> components = Arrays.asList(partString.split(","));
+            final Part part = new Part();
+            part.setChannel(channel);
+            final Phrase phrase = new Phrase();
             for (String comp : components) {
-                if (comp.toUpperCase().startsWith("FI")) {
-                    //It's a filename
-                    fileName = comp.substring(2) + ".mid";
-                } else if (comp.toUpperCase().startsWith("TI")) {
-                    //It's a title
-                    score.setTitle(comp.substring(2));
-                } else if (comp.toUpperCase().startsWith("I")) {
-                    //It's an instrument
-                    part.setInstrument(Integer.parseInt(comp.replaceAll("I", "")));
-                } else if (comp.toUpperCase().startsWith("T")) {
-                    //It's a tempo
-                    final int tempo = Integer.parseInt(comp.replaceAll("T", ""));
-                    score.setTempo(tempo);
-                } else {
-                    phrase.add(noteTranslator.translate(comp.toUpperCase()));
+                try {
+                    if (comp.toUpperCase().startsWith("FI")) {
+                        //It's a filename
+                        fileName = comp.substring(2) + ".mid";
+                    } else if (comp.toUpperCase().startsWith("TI")) {
+                        //It's a title
+                        score.setTitle(comp.substring(2));
+                    } else if (comp.toUpperCase().startsWith("I")) {
+                        //It's an instrument
+                        part.setInstrument(Integer.parseInt(comp.replaceAll("I", "")));
+                    } else if (comp.toUpperCase().startsWith("T")) {
+                        //It's a tempo
+                        final int tempo = Integer.parseInt(comp.replaceAll("T", ""));
+                        score.setTempo(tempo);
+                    } else {
+                        phrase.add(noteTranslator.translate(comp.toUpperCase()));
+                    }
+                } catch (Exception e) {
+                    throw new TranslationException(e);
                 }
             }
-
             part.add(phrase);
             score.add(part);
-
-            return new TranslationResponse(score, fileName);
-        } catch (Exception e) {
-            throw new TranslationException(e);
+            channel++;
         }
+
+        return new TranslationResponse(score, fileName);
+
     }
 }
